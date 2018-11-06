6ba886af76bfdddba3d41509c7e7e9d89a9a1699
kibbitTheKnight <kibbitintheknight@gmail.com>
2018-11-06 18:24:54 -0500

    Update solver.js

--- js/solver.js
+++ js/solver.js
@@ -1,7 +1,7 @@
 /* Keira Taylor and Ashley Smith
  * Graphics
- * Rubix Cube Solver
- * Solves Rubix cubes
+ * Rubiks Cube Solver
+ * Solves Rubiks cubes
 */
 
 
@@ -13,4 +13,43 @@ for(i = 0; i < 6; i++)
   faces.push(temp);
 }
 
+//Commands for rotating cube
 
+//--> Use for reference: https://rubiks-cu.be/
+
+//Right Clockwise
+var sides= [];
+sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
+var temp = faces.slice();
+for(i = 0; i < 4; i++)
+{
+ for(j = 1; j <= 3; j++)
+ {
+   if(i == 0)
+   {
+    faces[sides[sides.length - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1]; 
+   }
+   else
+   {
+     faces[sides[i - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1];
+   }
+ }
+}
+
+//Right Counterclockwise
+
+//Left Clockwise
+
+//Left Counterclockwise
+
+//Upper Clockwise
+
+//Upper Counterclockwise
+
+//Lower Clockwise
+
+//Lower Counterclockwise
+
+//Back Clockwise
+
+//Back Counterclockwise
