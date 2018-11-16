1606f76bdaacffb6105eb9ea622ac9800ebc5c26
Keira <kibbitintheknight@knights.ucf.edu>
2018-11-16 15:51:16 -0500

    Cube is now 3D array. Faces -> Rows -> Cols

--- js/solver.js
+++ js/solver.js
@@ -6,36 +6,53 @@
 
 
 //create cube with faces
-var faces = [];
-for(i = 0; i < 6; i++)
+var cube = [];
+for(f = 0; f < 6; f++)
 {
-  let temp = Array(9).fill(0);
-  faces.push(temp);
+	var faces = [];
+	for(i = 0; i < 3; i++)
+	{
+	let temp = Array(3).fill(f);
+	faces.push(temp);
+	}
+	cube.push(faces);
 }
 
-//For testing purposes: for(i = 0; i < 6; i++) {faces[i][8] = i + 1;}
+//For testing purposes: for(i = 0; i < 6; i++) {faces[i][8] = i + 1;} for(i = 0; i < 3; i++) {for(j = 0; j < 3; j++) {faces[i][j] = (i * 3) + j;}}
 
 //Commands for rotating cube
 
 //--> Use for reference: https://rubiks-cu.be/
 
-//Right Clockwise
 var sides= [];
-sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
-var temp = copyArray(faces);
-for(i = 0; i < 4; i++)
+
+//Right Clockwise
+function rw(cube)
 {
- for(j = 1; j <= 3; j++)
- {
-   if(i == 0)
-   {
-    faces[sides[sides.length - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1]; 
-   }
-   else
-   {
-     faces[sides[i - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1];
-   }
- }
+	/*sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
+	var temp = copyArray(faces);
+	for(i = 0; i < 4; i++)
+	{
+		for(j = 1; j <= 3; j++)
+		{
+			if(i == 0)
+			{
+				faces[sides[sides.length - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1]; 
+			}
+			else
+			{
+				faces[sides[i - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1];
+			}
+		}
+	}*/
+	tempFaces = copyArray(cube);
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			cube[3][i][j] = tempFaces[3][2 - j][i];
+		}
+	}
 }
 
 //Right Counterclockwise
@@ -60,19 +77,27 @@ for(i = 0; i < 4; i++)
 //helper functions
 function copyArray(myarray)
 {
- let rows = myarray.length;
- let cols = myarray[0].length;
-  
+ let faces = 6;
+ let rows = myarray[0].length;
+ let cols = myarray[0][0].length;
  let temp = [];
  
-  
- for(r = 0; r < rows; r++)
- {
-   temp.push(Array(cols));
-  for(c = 0; c < cols; c++)
+  for(f = 0; f < faces; f++)
   {
-    temp[r][c] = myarray[r][c];
+	  console.log("Faces: " + faces + " f: " + f);
+	var faceses = [];
+	//faceses.push(Array(rows));
+	for(r = 0; r < rows; r++)
+	{
+		faceses.push(Array(cols));
+		for(c = 0; c < cols; c++)
+		{
+			console.log(myarray[f][r][c]);
+			faceses[r][c] = myarray[f][r][c];
+		}
+	}
+	cube.push(faceses);
+	cube.splice(6, 4);
   }
- }
-  return temp;
+	return cube;
 }
