49ae77285404ffcd88ca66d472102e382441b32b
Keira <kibbitintheknight@knights.ucf.edu>
2018-11-18 15:37:50 -0500

    R, R', L, and L' now work

--- js/solver.js
+++ js/solver.js
@@ -27,24 +27,28 @@ for(f = 0; f < 6; f++)
 var sides= [];
 
 //Right Clockwise
-function rw(cube)
+function rcw(cube)
 {
-	/*sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
-	var temp = copyArray(faces);
+	sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
+	var temp = copyArray(cube);
+	
 	for(i = 0; i < 4; i++)
 	{
-		for(j = 1; j <= 3; j++)
+		if(i > 2)
 		{
-			if(i == 0)
+			for(j = 0; j < 3; j++)
 			{
-				faces[sides[sides.length - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1]; 
-			}
-			else
-			{
-				faces[sides[i - 1]][j * 3 - 1] = temp[sides[i]][j * 3 - 1];
+				cube[sides[i]][j][2] = temp[sides[0]][j][2];
 			}
 		}
-	}*/
+		else
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[i+1]][j][2];
+			}
+		}
+	}
 	tempFaces = copyArray(cube);
 	for(i = 0; i < 3; i++)
 	{
@@ -56,10 +60,103 @@ function rw(cube)
 }
 
 //Right Counterclockwise
+function rcc(cube)
+{
+	sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
+	var temp = copyArray(cube);
+	
+	for(i = 0; i < 4; i++)
+	{
+		if(i == 0)
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[3]][j][2];
+			}
+		}
+		else
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[i-1]][j][2];
+			}
+		}
+	}
+	tempFaces = copyArray(cube);
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			cube[3][i][j] = tempFaces[3][j][2 - i];
+		}
+	}
+}
 
 //Left Clockwise
+function lcw(cube)
+{
+	sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
+	var temp = copyArray(cube);
+	
+	for(i = 0; i < 4; i++)
+	{
+		if(i > 2)
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[0]][j][2];
+			}
+		}
+		else
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[i+1]][j][2];
+			}
+		}
+	}
+	tempFaces = copyArray(cube);
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			cube[1][i][j] = tempFaces[1][2 - j][i];
+		}
+	}
+}
 
 //Left Counterclockwise
+function lcc(cube)
+{
+	sides[0] = 0; sides[1] = 2; sides[2] = 5; sides[3] = 4;
+	var temp = copyArray(cube);
+	
+	for(i = 0; i < 4; i++)
+	{
+		if(i > 2)
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[0]][j][2];
+			}
+		}
+		else
+		{
+			for(j = 0; j < 3; j++)
+			{
+				cube[sides[i]][j][2] = temp[sides[i+1]][j][2];
+			}
+		}
+	}
+	tempFaces = copyArray(cube);
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			cube[1][i][j] = tempFaces[1][2 - j][i];
+		}
+	}
+}
 
 //Upper Clockwise
 
@@ -77,27 +174,5 @@ function rw(cube)
 //helper functions
 function copyArray(myarray)
 {
- let faces = 6;
- let rows = myarray[0].length;
- let cols = myarray[0][0].length;
- let temp = [];
- 
-  for(f = 0; f < faces; f++)
-  {
-	  console.log("Faces: " + faces + " f: " + f);
-	var faceses = [];
-	//faceses.push(Array(rows));
-	for(r = 0; r < rows; r++)
-	{
-		faceses.push(Array(cols));
-		for(c = 0; c < cols; c++)
-		{
-			console.log(myarray[f][r][c]);
-			faceses[r][c] = myarray[f][r][c];
-		}
-	}
-	cube.push(faceses);
-	cube.splice(6, 4);
-  }
-	return cube;
+	return JSON.parse(JSON.stringify(myarray));
 }
